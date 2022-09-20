a498770052ce3d30c6e22b3104137c09fa67ab70
manavk09 <mk1745@scarletmail.rutgers.edu>
2022-09-19 20:24:52 -0400

    added the ability to read from file

--- server.py
+++ server.py
@@ -23,10 +23,9 @@ def server():
     print ("[S]: Got a connection request from a client at {}".format(addr))
 
     # send a intro message to the client.  
-    with open("in-proj.txt", "r+") as file1:
+    with open("in-proj.txt", "r+") as inProj:
         # Reading form a file
-        #print(file1.read()) 
-        msg = file1.read()[::-1]
+        msg = inProj.read()[::-1]
         csockid.send(msg.encode('utf-8'))
 
     # Close the server socket
